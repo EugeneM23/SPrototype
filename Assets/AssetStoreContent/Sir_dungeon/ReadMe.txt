8bf5fadb3168f5a23483efa2850f78f1c46c3f5a
EugeneM23 <johnyanimation@gmail.com>
2025-05-15 14:15:18 +0300

    update

---
+++ Assets/AssetStoreContent/Sir_dungeon/ReadMe.txt
@@ -0,0 +1,9 @@
+For post processing:
+1. Add Post-process Layer to camera 
+2. Add Post-process Volume to camera
+3. Add /Effects/PostProcessingProfile to Profile in Post-process Volume
+
+
+
+URP note: 
+You can convert materials to URP by Menu Edit->Rednder Pipeline->Universal Render Pipeline->Upgrade Project Materials. But vertical green fog shader and fire particles will not work correctly.
---
+++ Assets/AssetStoreContent/Sir_dungeon/Scripts/Light_flicker.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+ [RequireComponent(typeof(Light))]
+ public class Light_flicker : MonoBehaviour
+ {
+ 
+     [Tooltip("Max light intensity ")]
+     [SerializeField, Min(0)] float maxIntensity = 17f;
+ 
+     [Tooltip("Min light intensity")]
+     [SerializeField, Min(0)] float minIntensity = 7f;
+ 
+     [Tooltip("Max frequency flicker (seconds)")]
+     [SerializeField, Min(0)] float maxFlickerFrequency = 0.5f;
+ 
+     [Tooltip("Min frequency flicker (seconds)")]
+     [SerializeField, Min(0)] float minFlickerFrequency = 0.5f;
+ 
+     [Tooltip("Strength of flicker")]
+     [SerializeField, Min(0)] float strength = 3f;
+ 
+     float baseIntensity;
+     float nextIntensity;
+ 
+     float flickerFrequency;
+     float timeOfLastFlicker;
+
+ 
+     private Light LightCur => GetComponent<Light>();
+ 
+ 
+     private void OnValidate()
+     {
+         if (maxIntensity < minIntensity) minIntensity = maxIntensity;
+         if (maxFlickerFrequency < minFlickerFrequency) minFlickerFrequency = maxFlickerFrequency;
+     }
+ 
+     private void Awake()
+     {
+         baseIntensity = LightCur.intensity;
+ 
+         timeOfLastFlicker = Time.time;
+     }
+ 
+     private void Update()
+     {
+         if (timeOfLastFlicker + flickerFrequency < Time.time)
+         {
+             timeOfLastFlicker = Time.time;
+             nextIntensity = Random.Range(minIntensity, maxIntensity);
+             flickerFrequency = Random.Range(minFlickerFrequency, maxFlickerFrequency);
+         }
+ 
+         Flicker();
+     }
+ 
+     private void Flicker()
+     {
+         LightCur.intensity = Mathf.Lerp(
+             LightCur.intensity,
+             nextIntensity, 
+             strength * Time.deltaTime
+         );
+     }
+ 
+     public void Reset()
+     {
+         LightCur.intensity = baseIntensity;
+     }
+ }
