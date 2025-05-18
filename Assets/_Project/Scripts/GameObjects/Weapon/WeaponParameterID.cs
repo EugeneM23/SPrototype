7ca395ff76e78f2b0eb5a3bb4a5e4302a46ee7ca
EugeneM23 <johnyanimation@gmail.com>
2025-05-18 19:26:54 +0300

    Update

---
+++ Assets/_Project/Scripts/GameObjects/Weapon/WeaponParameterID.cs
@@ -0,0 +1,17 @@
+namespace Gameplay
+{
+    public enum WeaponParameterID
+    {
+        FireRate,
+        Scatter,
+        FireRange,
+        Damage,
+        BulletSpeed,
+        CameraShakeDuration,
+        CameraShakeMagnitude,
+        ShellImpulse,
+        RecoilPower,
+        ProjectileCount,
+        MaxRicochetCount
+    }
+}
