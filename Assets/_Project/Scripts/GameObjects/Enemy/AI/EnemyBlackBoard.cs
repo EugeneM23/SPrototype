95bd1ef818941ffffe2ce422d916c4e11b1b3ae2
EugeneM23 <johnyanimation@gmail.com>
2025-05-20 20:42:27 +0300

    Update

--- Assets/_Project/Scripts/GameObjects/Enemy/AI/EnemyBlackBoard.cs
+++ Assets/_Project/Scripts/GameObjects/Enemy/AI/EnemyBlackBoard.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Zenject;
 
 namespace Gameplay
 {
@@ -22,5 +23,40 @@ namespace Gameplay
         public bool IsRunning;
         public Entity Target;
         public Entity Enemy;
+    }
+    
+    public class EnemyBlackBoardInstaller : MonoInstaller
+    {
+        [SerializeField] private float _chaseRange;
+        [SerializeField] private float _attackRange;
+        [SerializeField] private float _chaseSpeed;
+        [SerializeField] private float _patrolSpeed;
+        [SerializeField] private float _attackRotationSpeed;
+        [SerializeField] private int _damage;
+        [SerializeField] private int _health;
+        [SerializeField] private bool _isPushable;
+
+        public override void InstallBindings()
+        {
+            Container.Bind<float>().WithId(CharacterParameterID.ChaseRange).FromInstance(_chaseRange).AsCached();
+            Container.Bind<float>().WithId(CharacterParameterID.AttackRange).FromInstance(_attackRange).AsCached();
+            Container.Bind<float>().WithId(CharacterParameterID.ChaseSpeed).FromInstance(_chaseSpeed).AsCached();
+            Container.Bind<float>().WithId(CharacterParameterID.PatrolSpeed).FromInstance(_patrolSpeed).AsCached();
+            Container.Bind<float>().WithId(CharacterParameterID.AttackRotationSpeed).FromInstance(_attackRotationSpeed).AsCached();
+            Container.Bind<int>().WithId(CharacterParameterID.Damage).FromInstance(_damage).AsCached();
+            Container.Bind<int>().WithId(CharacterParameterID.Health).FromInstance(_health).AsCached();
+            Container.Bind<bool>().WithId(CharacterParameterID.IsPushable).FromInstance(_isPushable).AsCached();
+        }
+    }
+    public enum CharacterParameterID
+    {
+        ChaseRange,
+        AttackRange,
+        ChaseSpeed,
+        PatrolSpeed,
+        AttackRotationSpeed,
+        Damage,
+        Health,
+        IsPushable,
     }
 }
