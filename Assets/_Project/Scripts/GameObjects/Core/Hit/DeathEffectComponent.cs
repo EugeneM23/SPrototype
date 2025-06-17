8d31a888235722831ad2e211b267e6ef3c016b56
EugeneM23 <johnyanimation@gmail.com>
2025-06-17 16:05:09 +0300

    Update

--- Assets/_Project/Scripts/GameObjects/Core/Hit/DeathEffectComponent.cs
+++ Assets/_Project/Scripts/GameObjects/Core/Hit/DeathEffectComponent.cs
@@ -20,6 +20,8 @@ namespace Gameplay
             foreach (var deathEffect in _deathEffect)
             {
                 var effect = _factory.Create(deathEffect);
+                float randomRotation = Random.Range(-180f, 180f);
+                effect.transform.rotation = Quaternion.Euler(0f, randomRotation, 0f);
                 effect.transform.position = _hitPoint.position;
             }
         }
